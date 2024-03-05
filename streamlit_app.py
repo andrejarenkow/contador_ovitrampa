81b5ddf31806cf39e4d5114b65de5441979ff944
André Jarenkow <45919995+andrejarenkow@users.noreply.github.com>
2024-03-05 17:11:35 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,8 +4,8 @@ import numpy as np
 
 img_file_buffer = st.camera_input("Take a picture")
 
-min = st.number_input('min', value=100)
-max = st.number_input('max', value=200)
+min = st.slider('min', value=100, min_value=1, max_value=1000)
+max = st.slider('max', value=200, min_value=1, max_value=1000)
 
 if img_file_buffer is not None:
       # To read image file buffer with OpenCV:
